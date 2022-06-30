fb3717e1c10fc8dc8ecf0b7f779233d204b479b2
gogeruk <gogeruk_locker@protonmail.com>
2022-06-30 07:58:37 +0300

    start display

--- app/src/Controller/InstagramUserParserController.php
+++ app/src/Controller/InstagramUserParserController.php
@@ -2,26 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\InstagramUserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class InstagramUserParserController extends AbstractController
 {
-
-
-    #[Route('/parsers', name: 'app_instagram_user_parser')]
-    public function index(): Response
+    public function __construct
+    (
+        private EntityManagerInterface $em
+    )
     {
-        return $this->render('instagram_user_parser/index.html.twig', [
-            'controller_name' => 'InstagramUserParserController',
-        ]);
     }
 
-
-
-
-
+    /**
+     * @Route("/intagram/users", name="app_post_index")
+     */
+    public function index(InstagramUserRepository $instagramUserRepository): Response
+    {
+        return $this->render('instagram_user_parser/index.html.twig', [
+            'users' => $instagramUserRepository->findAll(),
+        ]);
+    }
 
 
 
